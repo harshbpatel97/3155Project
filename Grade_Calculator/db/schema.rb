4a93ca6aa150fbe213b785e3fed21976fdeb747b
Chase Blackwelder <mblack64@uncc.edu>
2019-11-19 15:43:58 +0000

    Created a folder for documentation

--- Grade_Calculator/db/schema.rb
+++ Grade_Calculator/db/schema.rb
@@ -47,7 +47,6 @@ ActiveRecord::Schema.define(version: 2019_11_17_084532) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "weightAvg"
     t.index ["user_id"], name: "index_grades_on_user_id"
   end
 
