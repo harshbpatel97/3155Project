ca35a5c1d9ff29d5f235e9e4380dd3f2c2055d42
Harshbpatel97 <hpatel97@uncc.edu>
2019-11-23 17:54:28 +0000

    Modified changes to enable result and weightAvg field

--- Grade_Calculator/app/controllers/grades_controller.rb
+++ Grade_Calculator/app/controllers/grades_controller.rb
@@ -11,6 +11,6 @@ class GradesController < ApplicationController
     
     
     def grade_params
-       params.require(:grade).permit(:assignment1,:assignment2,:assignment3,:assignment4,:assignment5,:assignment6,:assignment7,:assignment8,:assignment9,:assignment10, :gradeNum1, :gradeNum2, :gradeNum3, :gradeNum4, :gradeNum5, :gradeNum6, :gradeNum7, :gradeNum8, :gradeNum9, :gradeNum10, :wieght1, :weight2, :weight3, :weight4, :weight5, :weight6, :weight7, :weight8, :weight9, :weight10) 
+       params.require(:grade).permit(:assignment1,:assignment2,:assignment3,:assignment4,:assignment5,:assignment6,:assignment7,:assignment8,:assignment9,:assignment10, :gradeNum1, :gradeNum2, :gradeNum3, :gradeNum4, :gradeNum5, :gradeNum6, :gradeNum7, :gradeNum8, :gradeNum9, :gradeNum10, :wieght1, :weight2, :weight3, :weight4, :weight5, :weight6, :weight7, :weight8, :weight9, :weight10, :weightAvg, :result) 
     end
 end
